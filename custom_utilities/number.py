6f638a46fb5b4d1e2edc2f0b2af0d6ba5eb926d6
Agra Bima Yuda <161706119+agrayuda@users.noreply.github.com>
2024-08-30 23:35:38 +0700

    docs: Adjust Args information format

--- custom_utilities/number.py
+++ custom_utilities/number.py
@@ -2,7 +2,7 @@ def format_float(value:float, precision:int=3) -> str:
     """Return formatted float.
     
     Args:
-        value: A `float`. Float value to be formatted.
-        precision: An `Integer`. Desired precision of the float value. Defaults to `3`.
+        value (`float`, Mandatory): Float value to be formatted.
+        precision (`int`, Optional): Desired precision of the float value. Defaults to `3`.
     """
     return str(round(value,precision)).rstrip('0').rstrip('.')
