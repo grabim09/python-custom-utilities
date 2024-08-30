5f5ea718ef843234f9387131d67ee3844aab996c
Agra Bima Yuda <161706119+agrayuda@users.noreply.github.com>
2024-08-30 23:35:21 +0700

    docs: Adjust Args information format

--- custom_utilities/output.py
+++ custom_utilities/output.py
@@ -4,7 +4,7 @@ def clear_line(line:int=1):
     """Clear printed line in terminal or output.
 
     Args:
-        line: An `Integer`. Amount of line to be cleared from terminal or output. Defaults to `1`.
+        line (`int`, Optional): Amount of line to be cleared from terminal or output. Defaults to `1`.
     """
     for _ in range(line):
         sys.stdout.write("\033[F")
